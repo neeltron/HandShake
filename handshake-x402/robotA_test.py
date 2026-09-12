eb95233d36da6ee36350c55e4c0f0b849df0ca3b
Neel Adwani <nprosofficial@gmail.com>
2026-09-12 05:50:42 -0100

    Fixed library and package mismatch

--- handshake-x402/robotA_test.py
+++ handshake-x402/robotA_test.py
@@ -5,13 +5,13 @@ Created on Sat Sep 12 01:55:06 2026
 @author: neeltron
 """
 
-from pymycobot.mycobot280 import MyCobot280
+from pymycobot.mycobot import MyCobot
 import time
 
 PORT = "/dev/ttyUSB0"
 BAUDRATE = "1000000"
 
-mc = MyCobot280(PORT, BAUDRATE)
+mc = MyCobot(PORT, BAUDRATE)
 
 target_angles = [0, 0, 0, 0, 0, 0]
 speed = 50
@@ -19,8 +19,8 @@ speed = 50
 mc.send_angles(target_angles, speed)
 time.sleep(2)
 
-mc.send_angle(1, 45, speed)
+mc.send_angle(2, 0, speed)
 
 print("J1 min/max:", mc.get_joint_min_angle(1), mc.get_joint_max_angle(1))
 
-print("Current angles:", mc.get_angles())
+print("Current angles:", mc.get_angles)
